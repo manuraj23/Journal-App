3efabffbee3bc7ec18fbd321c9b4a9fa73df85ca
manuraj23 <manuraj082004@gmail.com>
2025-02-17 23:12:06 +0530

    Initial commit

---
+++ Projects/JournelApp/src/main/java/com/JournelApp/JournelApp/JournelAppApplication.java
@@ -0,0 +1,13 @@
+package com.JournelApp.JournelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournelAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JournelAppApplication.class, args);
+	}
+
+}
