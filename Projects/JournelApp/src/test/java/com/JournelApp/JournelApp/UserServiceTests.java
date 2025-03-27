f712ffab48ba4b9833077a067e091f18b7dd8e90
manuraj23 <manuraj082004@gmail.com>
2025-03-27 11:30:13 +0530

    Added JUnit testing

---
+++ Projects/JournelApp/src/test/java/com/JournelApp/JournelApp/UserServiceTests.java
@@ -0,0 +1,38 @@
+package com.JournelApp.JournelApp;
+
+
+import com.JournelApp.JournelApp.repositeries.UserRepository;
+import com.JournelApp.JournelApp.services.UserService;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class UserServiceTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+
+
+    @Disabled
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,2",
+            "2,10,12",
+            "3,3,9"
+    })
+    public void test(int a, int b, int expected){
+        assertEquals(expected, a + b);
+    }
+}
