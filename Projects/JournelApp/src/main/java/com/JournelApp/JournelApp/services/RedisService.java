eb76fd09c0678da69db70dbb595529b0e4b38e1b
Manu Raj <114493702+manuraj23@users.noreply.github.com>
2026-03-12 20:47:40 +0530

    Remove unnecessary blank line in RedisService

--- Projects/JournelApp/src/main/java/com/JournelApp/JournelApp/services/RedisService.java
+++ Projects/JournelApp/src/main/java/com/JournelApp/JournelApp/services/RedisService.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
 import java.util.concurrent.TimeUnit;
 @Service
 @Slf4j
